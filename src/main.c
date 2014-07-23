9474feb97575ea855778bbaa5f00f4fdd1f2a89f
ravaliB <benjamin.ravalijaona@gmail.com>
2014-07-23 19:17:00 +0200

    tpl added

--- src/main.c
+++ src/main.c
@@ -1,24 +1,39 @@
 #include "Trie.h"
+#include "mman.h"
+#include "tpl.h"
 
-int main()
+
+int main(int argc, char **argv)
 {
-    trieNode_t *root;
-    printf("Trie Example\n");
-     
-    /*Create a trie*/
-    TrieCreate(&root);
-     
-    TrieAdd(&root, "andrew", 1);
-    TrieAdd(&root, "tina", 2);
-    TrieAdd(&root, "argo", 3);
-    TrieAdd(&root, "timor", 5);
-    TrieRemove(&root, "tim");
-    TrieAdd(&root, "tim", 6);
-    TrieRemove(&root, "tim");
-    TrieAdd(&root, "ti", 6);
-    TrieAdd(&root, "amy", 7);
-    TrieAdd(&root, "aramis", 8);
- 
-    /*Destroy the trie*/
-    TrieDestroy(root);
+		trieNode_t *root;
+		FILE *file;
+		TPL_API tpl_node *tn;
+		int id;
+		
+		/*Create a trie*/
+		TrieCreate(&root);
+
+		TrieAdd(&root, "andrew", 1);
+		TrieAdd(&root, "tina", 2);
+		TrieAdd(&root, "argo", 3);
+		TrieAdd(&root, "timor", 5);
+		TrieRemove(&root, "tim");
+		TrieAdd(&root, "tim", 6);
+		TrieRemove(&root, "tim");
+		TrieAdd(&root, "ti", 6);
+		TrieAdd(&root, "amy", 7);
+		TrieAdd(&root, "aramis", 8);
+				
+		tn = tpl_map("S(ci$$$$)", &root);
+
+		for(id = 1; id < 9; id++) {
+			tpl_pack(tn,1);
+		}
+
+
+		tpl_dump(tn, TPL_FILE, "dico.bin");
+		tpl_free(tn);
+		/*Destroy the trie*/
+		TrieDestroy(root);
+	
 }
